b0abfad7d20e264a61fa093e2e646c5d2fe77cf4
areelvestad <areelvestad@yahoo.com>
2024-07-15 23:01:36 +0200

    ADDED 3D MAP OMG

---
+++ JS/explorePaths3D.js
@@ -0,0 +1,62 @@
+import { listPaths } from './listPaths.js';
+
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzYmIwMDE3My01NzBlLTQ2MGQtYTBlYS1lMmRmOGY5NjcxMTYiLCJpZCI6MjI4NTYwLCJpYXQiOjE3MjEwNzYxMjl9.w1AiAHTQP91nxRytXEy6M7MwJ41qWst1p8NgfygrWUE';
+
+const viewer = new Cesium.Viewer('cesiumContainer', {
+    terrainProvider: Cesium.createWorldTerrain(),
+    imageryProvider: Cesium.createWorldImagery(), 
+    animation: false,
+    timeline: false
+});
+
+viewer.scene.globe.depthTestAgainstTerrain = true;
+
+viewer.camera.setView({
+    destination: Cesium.Cartesian3.fromDegrees(21.215057647316442, 69.7245400796181, 15000), // 15000 meters height
+    orientation: {
+        heading: Cesium.Math.toRadians(0),
+        pitch: Cesium.Math.toRadians(-30),
+        roll: 0.0
+    }
+});
+
+const pinBuilder = new Cesium.PinBuilder();
+const routesPromise = [];
+
+listPaths.forEach(path => {
+    const promise = fetch(path.geojson)
+        .then(response => response.json())
+        .then(geojson => {
+            const geoJsonDataSource = Cesium.GeoJsonDataSource.load(geojson, {
+                stroke: Cesium.Color.RED, 
+                fill: Cesium.Color.RED.withAlpha(0.5), 
+                strokeWidth: 3,
+                clampToGround: true 
+            });
+
+            viewer.dataSources.add(geoJsonDataSource);
+            geoJsonDataSource.then(dataSource => {
+                const entities = dataSource.entities.values;
+                for (let entity of entities) {
+                    entity.billboard = new Cesium.BillboardGraphics({
+                        image: pinBuilder.fromText(path.name.charAt(0).toUpperCase(), Cesium.Color.RED, 48).toDataURL(),
+                        verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+                    });
+
+                    entity.description = `
+                        <h2>${path.name}</h2>
+                        <p>${path.description}</p>
+                        <p><b>Type:</b> ${path.type}</p>
+                        <p><b>Grade:</b> ${path.grade}</p>
+                    `;
+                }
+            });
+        })
+        .catch(error => console.error('Error loading GeoJSON:', error));
+
+    routesPromise.push(promise);
+});
+
+Promise.all(routesPromise).then(() => {
+    viewer.flyTo(viewer.dataSources);
+});
